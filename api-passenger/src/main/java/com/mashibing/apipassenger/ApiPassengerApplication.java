44d86d91ef6486f0b26a96fdb6d82941b7459341
ma.jiajian <1061902084@qq.com>
2024-12-16 16:51:11 +0800

    代码启东测试

---
+++ api-passenger/src/main/java/com/mashibing/apipassenger/ApiPassengerApplication.java
@@ -0,0 +1,18 @@
+package com.mashibing.apipassenger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @description
+ * @author 马佳健
+ * @date 16:33 2024/12/16
+ **/
+@SpringBootApplication
+public class ApiPassengerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiPassengerApplication.class);
+    }
+
+}
---
+++ api-passenger/src/main/java/com/mashibing/apipassenger/controller/TestController.java
@@ -0,0 +1,19 @@
+package com.mashibing.apipassenger.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+* @author 马佳健
+* @date 2024/12/16
+* @description
+*/
+@RestController
+public class TestController {
+
+    @GetMapping("/test")
+    public String test(){
+
+        return "test api passenger";
+    }
+}
