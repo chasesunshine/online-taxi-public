acbf2b39282a04d9170fee9e51af9894e32c4b42
ma.jiajian <1061902084@qq.com>
2024-12-17 20:27:02 +0800

    feign接口调用和配置

--- api-passenger/src/main/java/com/mashibing/apipassenger/service/VerificationCodeService.java
+++ api-passenger/src/main/java/com/mashibing/apipassenger/service/VerificationCodeService.java
@@ -1,12 +1,23 @@
 package com.mashibing.apipassenger.service;
 
+import com.mashibing.apipassenger.remote.ServiceVefificationcodeClient;
+import com.mashibing.internalcommon.dto.ResponseResult;
+import com.mashibing.internalcommon.responese.NumberCodeResponse;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class VerificationCodeService {
+    @Autowired
+    private ServiceVefificationcodeClient serviceVefificationcodeClient;
 
     public String generatorCode(String passengerPhone){
-        return null;
+        // 调用验证码服务，获取验证码
+        ResponseResult<NumberCodeResponse> numberCodeResponse = serviceVefificationcodeClient.getNumberCode(6);
+        int numberCode = numberCodeResponse.getData().getNumberCode();
+
+        // 通过短信服务商，将对应的验证码发送到手机上。阿里短信服务，腾讯短信通，华信，容联
+        return "";
     }
 
 }
