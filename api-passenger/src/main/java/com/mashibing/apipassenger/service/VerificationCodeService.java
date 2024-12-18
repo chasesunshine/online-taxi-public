2569711b6e903470379812a91934d21d4da25be6
ma.jiajian <1061902084@qq.com>
2024-12-18 15:47:11 +0800

    发送验证码 - 加入redis

--- api-passenger/src/main/java/com/mashibing/apipassenger/service/VerificationCodeService.java
+++ api-passenger/src/main/java/com/mashibing/apipassenger/service/VerificationCodeService.java
@@ -1,23 +1,37 @@
 package com.mashibing.apipassenger.service;
 
 import com.mashibing.apipassenger.remote.ServiceVefificationcodeClient;
+import com.mashibing.internalcommon.constant.IdentityConstants;
 import com.mashibing.internalcommon.dto.ResponseResult;
 import com.mashibing.internalcommon.responese.NumberCodeResponse;
+import com.mashibing.internalcommon.util.RedisPrefixUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
+
+import java.util.concurrent.TimeUnit;
 
 @Service
 public class VerificationCodeService {
     @Autowired
     private ServiceVefificationcodeClient serviceVefificationcodeClient;
 
-    public String generatorCode(String passengerPhone){
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+
+    public ResponseResult generatorCode(String passengerPhone){
         // 调用验证码服务，获取验证码
         ResponseResult<NumberCodeResponse> numberCodeResponse = serviceVefificationcodeClient.getNumberCode(6);
         int numberCode = numberCodeResponse.getData().getNumberCode();
 
+        // 存入redis
+        // key,value,过期时间
+        String key = RedisPrefixUtils.generatorKeyByPhone(passengerPhone, IdentityConstants.PASSENGER_IDENTITY) ;
+        // 存入redis
+        stringRedisTemplate.opsForValue().set(key,numberCode+"",2, TimeUnit.MINUTES);
+
         // 通过短信服务商，将对应的验证码发送到手机上。阿里短信服务，腾讯短信通，华信，容联
-        return "";
+        return ResponseResult.success("");
     }
 
 }
