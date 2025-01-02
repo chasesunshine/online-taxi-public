7d41db85c1fae54727a130c31a7792a78f8d597a
ma.jiajian <1061902084@qq.com>
2025-01-02 12:18:54 +0800

    双token刷新接口 /token-refresh

--- internal-common/src/main/java/com/mashibing/internalcommon/dto/TokenResult.java
+++ internal-common/src/main/java/com/mashibing/internalcommon/dto/TokenResult.java
@@ -2,10 +2,23 @@ package com.mashibing.internalcommon.dto;
 
 import lombok.Data;
 
+/**
+ * token解析返回结果
+ *
+* @author 马佳健
+* @date 2025/1/2
+* @description
+*/
 @Data
 public class TokenResult {
 
+    /**
+     * 电话号码
+     */
     private String phone;
 
+    /**
+     * 身份信息
+     */
     private String identity;
 }
--- internal-common/src/main/java/com/mashibing/internalcommon/responese/TokenResponse.java
+++ internal-common/src/main/java/com/mashibing/internalcommon/responese/TokenResponse.java
@@ -2,10 +2,23 @@ package com.mashibing.internalcommon.responese;
 
 import lombok.Data;
 
+/**
+ * token返回参数
+ *
+* @author 马佳健
+* @date 2025/1/2
+* @description
+*/
 @Data
 public class TokenResponse {
 
+    /**
+     * 请求token
+     */
     private String accessToken;
 
+    /**
+     * 刷新token
+     */
     private String refreshToken;
 }
