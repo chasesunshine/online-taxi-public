88386555982ef21d53c19a8e8a3dfb1b55bdc599
ma.jiajian <1061902084@qq.com>
2025-01-03 14:57:33 +0800

    预估价格 - 调用第三方地图接口（测试成功）

--- serice-map/src/main/java/com/mashibing/servicemap/service/DirectionService.java
+++ serice-map/src/main/java/com/mashibing/servicemap/service/DirectionService.java
@@ -2,13 +2,15 @@ package com.mashibing.servicemap.service;
 
 import com.mashibing.internalcommon.dto.ResponseResult;
 import com.mashibing.internalcommon.responese.DirectionResponse;
+import com.mashibing.servicemap.remote.MapDirectionClient;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class DirectionService {
 
-//    @Autowired
-//    private MapDirectionClient mapDirectionClient;
+    @Autowired
+    private MapDirectionClient mapDirectionClient;
 
     /**
      * 根据起点经纬度和终点经纬获取距离（米）和时长（分钟）
@@ -22,11 +24,8 @@ public class DirectionService {
     public ResponseResult driving(String depLongitude,String depLatitude, String destLongitude,String destLatitude){
 
         // 调用第三方地图接口
-//        DirectionResponse direction = mapDirectionClient.direction(depLongitude, depLatitude, destLongitude, destLatitude);
+        DirectionResponse direction = mapDirectionClient.direction(depLongitude, depLatitude, destLongitude, destLatitude);
 
-        DirectionResponse direction = new DirectionResponse();
-        direction.setDistance(123);
-        direction.setDuration(2);
         return ResponseResult.success(direction);
     }
 }
