175a96b67ebc71033f1e0c935d73a5e7c2f6b552
ma.jiajian <1061902084@qq.com>
2025-01-03 11:06:22 +0800

    地图服务模块搭建

---
+++ serice-map/src/main/java/com/mashibing/servicemap/service/DirectionService.java
@@ -0,0 +1,28 @@
+package com.mashibing.servicemap.service;
+
+import com.mashibing.internalcommon.dto.ResponseResult;
+import org.springframework.stereotype.Service;
+
+@Service
+public class DirectionService {
+
+//    @Autowired
+//    private MapDirectionClient mapDirectionClient;
+
+    /**
+     * 根据起点经纬度和终点经纬获取距离（米）和时长（分钟）
+     *
+     * @param depLongitude
+     * @param depLatitude
+     * @param destLongitude
+     * @param destLatitude
+     * @return
+     */
+    public ResponseResult driving(String depLongitude,String depLatitude, String destLongitude,String destLatitude){
+
+        // 调用第三方地图接口
+//        DirectionResponse direction = mapDirectionClient.direction(depLongitude, depLatitude, destLongitude, destLatitude);
+
+        return ResponseResult.success(null);
+    }
+}
